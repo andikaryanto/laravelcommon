89edb89d59b1b4a09f6cf9d93a3c3545d935237a
Andik <andik.aryantoo@gmail.com>
2026-02-25 16:46:13 +0000

    KAN-172 non tenant base model

--- src/App/Models/BaseModel.php
+++ src/App/Models/BaseModel.php
@@ -2,69 +2,9 @@
 
 namespace LaravelCommon\App\Models;
 
-use Illuminate\Database\Eloquent\Model;
-use LaravelCommon\App\Database\Eloquent\Relations\BelongsToManyRelation;
-use LaravelCommon\App\Database\Eloquent\Relations\BelongsToRelation;
-use LaravelCommon\App\Database\Eloquent\Relations\HasManyRelation;
-use LaravelCommon\App\Database\Eloquent\Relations\HasOneRelation;
-use ReflectionClass;
-use ReflectionProperty;
 use Spatie\Multitenancy\Models\Concerns\UsesTenantConnection;
 
 class BaseModel extends Model
 {
-    use AuditableModel;
     use UsesTenantConnection;
-
-    public function __construct(array $attributes = [])
-    {
-        $reflectionClass = new ReflectionClass($this);
-        $properties = $reflectionClass->getProperties(ReflectionProperty::IS_PROTECTED);
-        foreach ($properties as $property) {
-            if (
-                $property->getType() &&
-                (
-                    $property->getType()->getName() == BelongsToRelation::class ||
-                    $property->getType()->getName() == HasOneRelation::class ||
-                    $property->getType()->getName() == HasManyRelation::class ||
-                    $property->getType()->getName() == BelongsToManyRelation::class
-                )
-            ) {
-                $propName = $property->getName();
-                $this->$propName->setName($propName);
-            }
-        }
-
-        parent::__construct($attributes);
-    }
-
-    /**
-     *
-     * @return mixed
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     *
-     * @param mixed $id
-     * @return $this
-     */
-    public function setId($id)
-    {
-        $this->id = $id;
-        return $this;
-    }
-
-    public function __call($method, $parameters)
-    {
-        $isRelationFound = $this->callRelationFound($method);
-        if ($isRelationFound) {
-            return $this->$method->getRelation();
-        } else {
-            return parent::__call($method, $parameters);
-        }
-    }
 }
