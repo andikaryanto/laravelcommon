dd1ecfb8379abae6ea1d6bf4f8c6bf612999c2cc
Andik <andik.aryantoo@gmail.com>
2026-03-11 18:17:57 +0000

    KAN-216 query where key

--- src/App/Queries/PricingApplicationFeatureQuery.php
+++ src/App/Queries/PricingApplicationFeatureQuery.php
@@ -17,4 +17,22 @@ class PricingApplicationFeatureQuery extends Query
         $this->where('pricing_id', $pricing->getId());
         return $this;
     }
+
+    public function whereApplicationFeatureKey(string $featureKey): PricingApplicationFeatureQuery
+    {
+        $this->joinWith(
+            'application_features',
+            'application_features.id',
+            '=',
+            'pricing_application_features.application_feature_id'
+        );
+        $this->where('application_features.key', $featureKey);
+        return $this;
+    }
+
+    public function whereIsActive(bool $isActive = true): PricingApplicationFeatureQuery
+    {
+        $this->where('pricing_application_features.is_active', $isActive);
+        return $this;
+    }
 }
